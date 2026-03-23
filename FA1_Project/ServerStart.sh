eb9216b1cb092ed6b6582014562cb8a9bcc09464
ThomasM249 <22MorriT@sbc.qld.edu.au>
2026-03-23 11:06:03 +0000

    Final for Duedate

--- FA1_Project/ServerStart.sh
+++ FA1_Project/ServerStart.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# Start a simple HTTP server to serve static files in the background
 python3 -m http.server 8000 &
 
-# Wait a moment for the server to start
 sleep 1
 
-# Open the server in the user's primary browser
 "$BROWSER" http://localhost:8000
-
-#To run the Script run ./ServerStart.sh in the Terminal.
